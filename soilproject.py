459b740f517172a963132f1ea8feb2a838abd2cf
lionturkey <tunkleo@umich.edu>
2025-04-20 16:39:45 -0400

    Organize runs better, start to merge branches

--- soilproject.py
+++ soilproject.py
@@ -2,7 +2,6 @@ from pathlib import Path
 import pandas as pd
 import matplotlib.pyplot as plt
 import becquerel as bq
-from lxml import etree as ET
 from bs4 import BeautifulSoup
 import numpy as np
 
@@ -43,11 +42,9 @@ def read_n42(file: Path) -> bq.Spectrum:
 def main():
     print("Hello from 586projectdata!")
     # get n42 files
-    background_n42_files = get_n42_paths("Background_RAW")
-    calibration_n42_files = get_n42_paths("Energy_Cal_RAW")
-    open_pit_mine_n42_files = get_n42_paths("Open_Pit_Mine_RAW")
-
-    # print(background_n42_files)
+    background_n42_files = get_n42_paths("background")
+    calibration_n42_files = get_n42_paths("energy_calibration")
+    open_pit_mine_n42_files = get_n42_paths("soil_openpit")
 
     # read n42 files
     background_spectra = [read_n42(file) for file in background_n42_files]
@@ -58,21 +55,24 @@ def main():
     graphing_folder = Path.cwd() / "graphs"
     graphing_folder.mkdir(exist_ok=True)
 
-    for index in range(8):
-        # print(background_spectra[index])
-        plt.plot(background_spectra[index][0], background_spectra[index][1])
-        plt.savefig(graphing_folder / f"background{index}.png")
-        plt.close()
 
-        # plot calibration spectra
-        plt.plot(calibration_spectra[index][0], calibration_spectra[index][1])
-        plt.savefig(graphing_folder / f"calibration{index}.png")
-        plt.close()
 
-        # plot open pit mine spectra
-        plt.plot(open_pit_mine_spectra[index][0], open_pit_mine_spectra[index][1])
-        plt.savefig(graphing_folder / f"open_pit_mine{index}.png")
-        plt.close()
+
+    # for index in range(8):
+    #     # print(background_spectra[index])
+    #     plt.plot(background_spectra[index][0], background_spectra[index][1])
+    #     plt.savefig(graphing_folder / f"background{index}.png")
+    #     plt.close()
+
+    #     # plot calibration spectra
+    #     plt.plot(calibration_spectra[index][0], calibration_spectra[index][1])
+    #     plt.savefig(graphing_folder / f"calibration{index}.png")
+    #     plt.close()
+
+    #     # plot open pit mine spectra
+    #     plt.plot(open_pit_mine_spectra[index][0], open_pit_mine_spectra[index][1])
+    #     plt.savefig(graphing_folder / f"open_pit_mine{index}.png")
+    #     plt.close()
 
     
 
